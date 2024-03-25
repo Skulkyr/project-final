0b2b4c8a5c535c859afddf1a28df7bc2ddaed4d0
Alexey <skulkyr20@gmail.com>
2024-03-25 09:58:02 +0300

    complete 3 task

--- src/test/java/com/javarush/jira/BaseTests.java
+++ src/test/java/com/javarush/jira/BaseTests.java
@@ -2,8 +2,12 @@ package com.javarush.jira;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb"
+})
 abstract class BaseTests {
 }
